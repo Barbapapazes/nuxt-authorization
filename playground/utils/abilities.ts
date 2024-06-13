2b5bc78cf9d862cf94ef5d20d236f30a4fafd0e5
barbapapazes <e.soubiran25@gmail.com>
2024-06-13 10:20:45 +0200

    chore: remove log

--- playground/utils/abilities.ts
+++ playground/utils/abilities.ts
@@ -6,6 +6,5 @@ export const listProducts = defineAbility({ allowGuest: true }, () => true)
 export const createProduct = defineAbility(() => true)
 
 export const editProduct = defineAbility((user: User, product: Product) => {
-  console.log(user, product)
   return user.id === product.ownerId
 })
