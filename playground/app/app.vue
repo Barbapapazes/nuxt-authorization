4273dcf35fb3c4cd1da76dbf5efa910c34ff8f99
barbapapazes <e.soubiran25@gmail.com>
2024-10-03 15:35:53 +0200

    refactor: avoid blink and add reactivity to bouncer component

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -2,17 +2,25 @@
 import type { Product } from '~~/shared/types/product'
 import { createProduct, editProduct, deleteProduct } from '~~/shared/abilities'
 
-const product: Product = {
+const product = ref<Product>({
   id: 1,
   name: 'Product 1',
   price: 100,
   ownerId: 1,
-}
+})
 </script>
 
 <template>
   <div>
     Nuxt Authorization Playground
+
+    <!-- This is used to test the reactivity -->
+    <label for="ownerId">Owner ID</label>
+    <input
+      id="ownerId"
+      v-model="product.ownerId"
+      type="number"
+    >
 
     <Can
       :bouncer-ability="createProduct"
